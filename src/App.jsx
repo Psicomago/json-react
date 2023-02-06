7304754b0b3e481f20fe613ac59fced4f333d892
Psicomago <carlosaravena.coach@gmail.com>
2023-02-05 22:47:39 -0300

    applying styles to the product container

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ const App = () => {
     return (
         <ProductContainer>
             <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            
         </ProductContainer>
     )
 }
