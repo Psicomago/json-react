2593df5d46b101c5476e955d61e6277edb2a73b2
Psicomago <carlosaravena.coach@gmail.com>
2023-02-05 23:50:28 -0300

    consuming a json of products

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
 import ProductContainer from "./components/ProductContainer";
 import { Product } from "./components/Product";
+import data from './components/products.json';
 
 const App = () => {
 
     return (
         <ProductContainer>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
+            {data.map(product=>
+            <Product 
+                key={product.key}
+                name={product.name}
+                oldPrice={product.price}
+                newPrice={product.price - (product.price * product.discount / 100)}
+            />
+            )}
             
         </ProductContainer>
     )
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,13 @@
 import "./Product.css"
 import { AddCartBtn } from "./AddCartBtn"
-const Product = () => {
+const Product = (props) => {
   return (
     <div className="product">
         <img className="product__img" src="https://picsum.photos/180" alt="Productos"/>
-        <h2 className="product__name">Camisa</h2>
+        <h2 className="product__name">{props.name}</h2>
         <div className="product__details">
-            <p className="product__oldPrice">$200</p>
-            <p className="product__Price">$100</p>
+            <p className="product__oldPrice">{props.oldPrice}</p>
+            <p className="product__Price">{props.newPrice}</p>
             <AddCartBtn/>
         </div>
         
