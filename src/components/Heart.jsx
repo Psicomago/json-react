4a2e69337d3062d9bd8426093ffa1559c0e13ac5
Psicomago <carlosaravena.coach@gmail.com>
2023-02-06 01:42:13 -0300

    creation of component like

---
+++ src/components/Heart.jsx
@@ -0,0 +1,19 @@
+import './Heart.css'
+import { FaHeart } from 'react-icons/fa';
+import { FaRegHeart } from 'react-icons/fa';
+import { useState } from 'react';
+
+const Heart = () => {
+
+    const [liked, setLiked] = useState(false)
+
+    const handleClick = () => {
+        liked == true ? setLiked(false) : setLiked(true)
+    }
+    return (
+        <div onClick={handleClick}>
+            {liked == true ? <FaHeart className='heart'/> : <FaRegHeart className='heart' />}
+        </div>
+    )
+}
+export {Heart}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,13 @@
 import "./Product.css";
 import { AddCartBtn } from "./AddCartBtn";
 import { Discount } from "./Discount";
+import { Heart } from "./Heart";
 
 const Product = (props) => {
   return (
     <div className="product">
         <Discount discount={props.discount}/>
+        <Heart/>
         <img className="product__img" src="https://picsum.photos/180" alt="Productos"/>
         <h2 className="product__name">{props.name}</h2>
         <div className="product__details">
